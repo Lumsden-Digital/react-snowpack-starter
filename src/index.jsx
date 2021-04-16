dc889c6e0b2a078837ef8d437079fcb13612181f
Lumsden Ruaidhri (BICS) <LumsdenR@sussex.nhs.uk>
2021-04-16 09:24:03 +0100

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+const root = document.getElementById('root')
+ReactDOM.render(<App />,root)
+
+// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+ // Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
+ if (import.meta.hot) {
+    import.meta.hot.accept();
+  }
