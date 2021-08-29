df93f65a6613b114fbb12c70542dc94b133af859
Ruaidhri Lumsden <roarbot@pop-os.localdomain>
2021-08-29 19:32:37 +0100

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import './assets/main.css'
+
 
 const App = () => {
     return (
         <div>
-            
+            Hello there!
         </div>
     )
 }
